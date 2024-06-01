eb07687a66444ea7932f615ff888dcab4a796be6
aljaffarsyah <222011596@stis.ac.id>
2024-06-01 18:17:55 +0700

    add server and dockerfile

---
+++ server.js
@@ -0,0 +1,5 @@
+const CubejsServer = require("@cubejs-backend/server");
+const server = new CubejsServer();
+server.listen().then(({ version, port }) => {
+  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
+});
